820b8ac796294861dd774b2ac7cede39473f245c
luis rodriguez <kike.lerj2710@gmail.com>
2021-09-30 12:41:24 -0500

    practicas con localStorage

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//variables
+const formulario = document.querySelector('#formulario');
+const listaTweests = document.querySelector('#lista-tweets');
+
+
+//eventos
+addEventListener();
+function addEventListener() {
+    
+}
+
+//funciones
