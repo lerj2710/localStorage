9e74595af51f9926f3df452a201b9cb6f015897c
luis rodriguez <kike.lerj2710@gmail.com>
2021-09-30 13:25:01 -0500

    agregando mesanje de error y validando el formulario

--- js/app.js
+++ js/app.js
@@ -1,12 +1,36 @@
 //variables
 const formulario = document.querySelector('#formulario');
 const listaTweests = document.querySelector('#lista-tweets');
-
+let tweets = [];
 
 //eventos
 addEventListener();
 function addEventListener() {
-    
+    formulario.addEventListener('submit', agregarTweet)
 }
 
 //funciones
+
+
+function agregarTweet(e) {
+    e.preventDefault();
+    const tweet = document.querySelector('#tweet').value;
+    if(tweet === ''){
+        mostrarError('No puede ir vacio');
+        return;
+    }
+     console.log('s hay algo');
+}
+
+function mostrarError(message) {
+    // crear el elemento para mostrar el mensaje de error
+    const messageError = document.createElement('p');
+        messageError.textContent = message;
+        messageError.classList.add('error');
+    const contenido = document.querySelector('#contenido');
+        contenido.appendChild(messageError);
+    // Eliminar el mesaje de alerta despues e 3 segundos
+        setTimeout(() => {
+    messageError.remove()
+}, 3000);
+}
